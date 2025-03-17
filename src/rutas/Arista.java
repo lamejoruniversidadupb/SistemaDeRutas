2eb8bc86cae47b37861323dcad0374adbf88b17f
lamejoruniversidadupb <andresnunez@192.168.1.8>
2025-03-16 23:11:52 -0500

    Revert "arreglos"
    This reverts commit 484a475a5518a66201e0dab712984828deb329f3.

--- src/rutas/Arista.java
+++ src/rutas/Arista.java
@@ -1,30 +1,40 @@
 package rutas;
 
-public class Arista {
-	private String Origen;
-	private String Destino;
-	private int distancia;
-	
-	
-	public String getOrigen() {
-		return Origen;
-	}
-	public void setOrigen(String origen) {
-		Origen = origen;
-	}
-	public String getDestino() {
-		return Destino;
-	}
-	public void setDestino(String destino) {
-		Destino = destino;
-	}
-	public int getDistancia() {
-		return distancia;
-	}
-	public void setDistancia(int distancia) {
-		this.distancia = distancia;
-	}
-	
-	
+import java.util.Objects;
 
+public class Arista {
+    private final String destino;
+    private final int distancia;
+
+    public Arista(String destino, int distancia) {
+        this.destino = destino;
+        this.distancia = distancia;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Arista arista = (Arista) o;
+        return distancia == arista.distancia && Objects.equals(destino, arista.destino);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(destino, distancia);
+    }
+
+    @Override
+    public String toString() {
+        return "Arista{destino='" + destino + "', distancia=" + distancia + "}";
+    }
 }
+
